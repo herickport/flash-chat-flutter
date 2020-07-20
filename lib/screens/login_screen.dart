b803cece49231857917e36547c038d91b3313676
Herick Portugues <herick.portugues@gmail.com>
2020-07-20 17:34:25 -0300

    Authenticating Users with Firebase

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:flash_chat/models/user.dart';
+import 'package:flash_chat/services/user_manager.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,6 +10,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,15 +25,13 @@ class _LoginScreenState extends State<LoginScreen> {
               Hero(
                 tag: 'logo',
                 child: Container(
-                  height: 200.0,
+                  height: 150.0,
                   child: Image.asset('images/logo.png'),
                 ),
               ),
               SizedBox(height: 48.0),
               TextField(
-                onChanged: (value) {
-                  //Do something with the user input.
-                },
+                controller: _emailController,
                 decoration: kTextFieldDecoration.copyWith(
                   labelText: 'Enter your email',
                 ),
@@ -36,9 +39,7 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               SizedBox(height: 16.0),
               TextField(
-                onChanged: (value) {
-                  //Do something with the user input.
-                },
+                controller: _passwordController,
                 decoration: kTextFieldDecoration.copyWith(
                   labelText: 'Enter your password',
                 ),
@@ -50,8 +51,15 @@ class _LoginScreenState extends State<LoginScreen> {
                 child: RoundedButton(
                   title: 'Log In',
                   color: Colors.lightBlueAccent,
-                  onPressed: () {
-                    Navigator.of(context).pop();
+                  onPressed: () async {
+                    User user = User(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                    );
+
+                    await UserManager().signIn(user);
+
+                    Navigator.of(context).pushReplacementNamed('/chat');
                   },
                 ),
               ),
